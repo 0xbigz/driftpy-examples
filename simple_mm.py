59e47d565b1aec61bc5d324e04943c5c0ff29076
Sameer Lal <sjl328@cornell.edu>
2023-04-29 23:54:27 -0400

    checkpoint - loop working with bid / ask adjustments

--- simple_mm.py
+++ simple_mm.py
@@ -12,6 +12,7 @@ from driftpy.types import *
 #MarketType, OrderType, OrderParams, PositionDirection, OrderTriggerCondition
 
 from driftpy.clearing_house import ClearingHouse
+from driftpy.clearing_house_user import ClearingHouseUser
 from driftpy.constants.numeric_constants import BASE_PRECISION, PRICE_PRECISION
 from borsh_construct.enum import _rust_enum
 
@@ -19,9 +20,10 @@ import requests, json
 import pandas as pd
 
 
-ORDERBOOK_URL_DEVNET = "https://master.dlob.drift.trade/orders/json"
-
-
+URL_DEVNET_OB = "https://master.dlob.drift.trade/orders/json"
+MARKET_INDEX_SOL = 0
+userAccount = "ECbtn9Y34m6L89EBDMRYxhpyrFgSHEcbELgozpqAvcDY"
+PKEY = "7f6d3DWGkNrKTERtW9McbBQCVbZibuHUf7CsdUjJyK1t"
 
 
 @_rust_enum
@@ -31,37 +33,159 @@ class PostOnlyParams:
     MUST_POST_ONLY = constructor()
 
 
-def get_order_book(market_index):
-    """
-        returns orderbook snapshot for market_index
-    
+
+
+def orderbook_all(perp_market="SOL-PERP"):
+    """ Queries DEVNET orderbok for specified perp market, organizes data into bid/asks sorted
+
     """
     try:
-        r = requests.get(ORDERBOOK_URL_DEVNET).json()
+        r = requests.get(URL_DEVNET_OB).json()
     except:
-        raise Exception("Error retrieving orderbook")
+        print(r)
+        raise Exception("Unable to fetch orderbook")
+
+    slot    = r['slot']
+    oracles = r['oracles']
+    orders  = r['orders']
+
+    ## market index -> price
+    market_to_oracle_map = pd.DataFrame(r['oracles']).set_index('marketIndex').to_dict()['price']
+
+    ## Get list of outstanding orders    
+    orders_all = list(map(lambda d: {"user":d['user'][:10], **d.pop('order')}, orders))
+    df = pd.DataFrame(orders_all)
+    df_raw = pd.DataFrame(orders_all)
     
-    market_to_oracle_map = pd.DataFrame(r['oracles']).set_index()
+    ## Only interested in (a) SOL-PERP (b) limit orders
+    # SOL is marketINdex=0
+    df = df[(df.marketIndex == MARKET_INDEX_SOL) & (df.marketType=='perp')]
+    
+    df["oraclePrice"] = df["marketIndex"].apply(lambda x: market_to_oracle_map.get(x, None))
+
+    ## Convert from lamports:
+    for col in ['price', 'oraclePrice', 'oraclePriceOffset']:
+        df[col] = df[col].astype(int)
+        df[col] /= 1e6
+
+    for col in ['quoteAssetAmountFilled']:
+        df[col] = df[col].astype(int)
+        df[col] /= 1e6 
+
+    for col in ['baseAssetAmount', 'baseAssetAmountFilled']:
+        df[col] = df[col].astype(int)
+        df[col] /= 1e9
+        
+    
+    bid_df = df[(df.direction == 'long') & (df.orderType == 'limit')]
+    ask_df = df[(df.direction == 'short')   &( df.orderType == 'limit')]
+    
+    ## Fixed prices are ok
+    ## Floating prices need to be marked to a fixed price according to oracle price    
+    bid_float = bid_df.loc[bid_df.price == 0] # inv: 0 price <-> floating order .w.r.t oracle 
+    ask_float = ask_df.loc[ask_df.price == 0] #
+    
+    bid_df.loc[bid_df.price == 0, "price"] = bid_float["oraclePrice"] + bid_float["oraclePriceOffset"]
+    ask_df.loc[ask_df.price == 0, "price"] = ask_float["oraclePrice"] + ask_float["oraclePriceOffset"]
+    
+    
+    bid_df = bid_df.sort_values(['price'], ascending=False)
+    ask_df = ask_df.sort_values(['price'])
+    
+    bid_df = bid_df.reset_index(drop=True)
+    ask_df = ask_df.reset_index(drop=True)
+    
+    return df_raw, bid_df, ask_df
+
+
+def orderbook_expanded(perp_market="SOL-PERP"):
+    """
+        Simplified View of Orderbook, by order
+    """
+    _, bid, ask = orderbook_all(perp_market)
+    
+    
+    
+    bid_simple = bid[["price", "baseAssetAmount", "baseAssetAmountFilled", "postOnly", "oraclePriceOffset", "oraclePrice"]]
+    ask_simple = ask[["price", "baseAssetAmount", "baseAssetAmountFilled", "postOnly", "oraclePriceOffset", "oraclePrice"]]
+
+    #### SOME ARE PARTIALLY FILLED
+    bid_simple["qty"] = bid_simple["baseAssetAmount"] - bid_simple["baseAssetAmountFilled"]
+    ask_simple["qty"] = ask_simple["baseAssetAmount"] - ask_simple["baseAssetAmountFilled"]
     
 
+    return bid_simple, ask_simple
+
+def get_dlob(perp_market="SOL-PERP"):
+    """
+        L2 orderbook view (only price / quantities)
+    """
+    bid, ask = orderbook_expanded(perp_market)
+    
+    bid_ob = bid[["price", "qty"]].groupby("price", sort=False).sum()
+    ask_ob = ask[["price", "qty"]].groupby("price", sort=False).sum()
+    return bid_ob, ask_ob
     
 
+def dlob_metrics(bid_ob, ask_ob, oraclePrice=None):
+    """
+        Returns various metrics on L2 orderbook, either in absolute terms or scaled by oraclePrice 
+    """
+    tob = bid_ob.iloc[:3].reset_index()
+    toa = ask_ob.iloc[:3].reset_index()
 
-def order_print(orders: list[OrderParams], market_str=None):
-    for order in orders:
-        if order.price == 0:
-            pricestr = '$ORACLE'
-            if order.oracle_price_offset > 0:
-                pricestr += ' + '+str(order.oracle_price_offset/1e6)
-            else:
-                pricestr += ' - '+str(abs(order.oracle_price_offset)/1e6)
-        else:
-            pricestr = '$' + str(order.price/1e6)
+    if(oraclePrice is None):
+        return {"mid"        : (bid_ob.index.max() + ask_ob.index.min() ) / 2.0,
+                "bid"        :  bid_ob.index.max(),
+                "ask"        :  ask_ob.index.min(),
+                "bid_avg"    : (tob["price"] * tob["qty"]).sum() / tob["qty"].sum(),
+                "ask_avg"    : (toa["price"] * toa["qty"]).sum() / toa["qty"].sum()
 
-        if market_str == None:
-            market_str = configs['mainnet'].markets[order.market_index].symbol
+        }
+    else:
+        return {"mid"       : (bid_ob.index.max() + ask_ob.index.min() ) / 2.0 - oraclePrice,
+                "bid"       :  bid_ob.index.max() - oraclePrice,
+                "ask"       :  ask_ob.index.min() - oraclePrice,
+                "bid_avg"   : (tob["price"] * tob["qty"]).sum() / tob["qty"].sum() - oraclePrice,
+                "ask_avg"   : (toa["price"] * toa["qty"]).sum() / toa["qty"].sum() - oraclePrice,
+        }
+    
 
-        print(str(order.direction).split('.')[-1].replace('()',''), market_str, '@', pricestr)
+async def get_solana_perp_oracle():
+    """ return SOL-PERP oracle """
+    with open(os.path.expanduser("/home/sjl/.config/solana/key.json"), 'r') as f: secret = json.load(f)
+    kp = Keypair.from_secret_key(bytes(secret))
+    subaccount = 0
+    print('using public key:', kp.public_key, " subaccount = ", subaccount)
+
+    env = 'devnet'
+    config = configs[env]
+    wallet = Wallet(kp) 
+    connection = AsyncClient(config.default_http)
+    provider = Provider(connection, wallet)
+
+    ch = ClearingHouse.from_config(config, provider)
+    chu = ClearingHouseUser(ch, authority=ch, subaccount_id=subaccount)
+    perp_market = await chu.get_perp_market(0)
+    oracle = (await chu.get_perp_oracle_data(perp_market)).price / PRICE_PRECISION
+
+    return oracle
+
+
+
+
+
+bid_offsets = [-10, -12, -15]
+bid_qtys    = [1.1, 2.2, 1.1]
+
+ask_offsets = [10, 12, 15]
+ask_qtys    = [1.1, 2.2, 1.1]
+
+# await construct_and_place(bid_offsets, bid_qtys, ask_offsets, ask_qtys)
+
+
+
+
 
 
 async def main(
